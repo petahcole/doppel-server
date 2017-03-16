e6ca4ca517f9cc47c8434e8ff91d6a3f920aeef6
petahcole <cbatson89@gmail.com>
2017-03-16 10:44:32 -0600

    go server work

--- main.go
+++ main.go
@@ -1,23 +1,25 @@
 package main
 
-import {
+import (
 	"fmt"
-	"io"
-	"github.com/gin-gonic/gin"
+	"os"
+	// "io"
+	// "github.com/gin-gonic/gin"
     "gopkg.in/mgo.v2"
-    "gopkg.in/mgo.v2/bson"
-    "net/http"
-}
+    // "gopkg.in/mgo.v2/bson"
+    // "net/http"
+)
 
 func main()	{
 
 	session, err := mgo.Dial("localhost")
-	userCol := session.DB("doppel").C("users")
-	wholeCol := session.DB("doppel").C("wholesaler")
+	// userCol := session.DB("doppel").C("users")
+	// wholeCol := session.DB("doppel").C("wholesaler")
 	if err != nil	{
 		panic(err)
 	}
 	
+	// session.Copy()
 	defer session.Close()
 
 	port := os.Getenv("PORT")
@@ -25,7 +27,7 @@ func main()	{
     port = "3000"
   	}
 
-
+	fmt.Println("hello from go")
   
   
 
--- routes.go
+++ routes.go
@@ -1,25 +1,40 @@
 package main
 
-import {
+import (
 	"fmt"
-	"io"
+	// "io"
 	"github.com/gin-gonic/gin"
     "gopkg.in/mgo.v2"
     "gopkg.in/mgo.v2/bson"
     "net/http"
 	"golang.org/x/crypto/bcrypt"
 
-
-}
+)
 
 type User struct	{
 	email string
 	password string
 }
 
+const (
+    MinCost     int = 4  
+    MaxCost     int = 31 
+    DefaultCost int = 10 
+)
 
 r := gin.Default()
 
 r.POST("/login", func(c *gin.Context)	{
+	//parse the json object coming in?
+	email := c.User("email")
+	password := c.User("password")
+	hashedPass, err := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
+		if err != nil	{
+			panic(err)
+		}
+	func isCorrectPassword(email *User, password []byte) boolean {
+    hashedPassword := user.HashedPassword()
+    return bcrypt.CompareHashAndPassword(hashedPassword, password) == nil
+}
 
 })
